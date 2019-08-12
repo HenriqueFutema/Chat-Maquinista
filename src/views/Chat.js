5a4a995d6a02e8b7040cb75c898ff19f726db912
HenriqueFutema <minifut0407@gmail.com>
2019-08-12 14:27:19 -0300

    Commit inicial

---
+++ src/views/Chat.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+export default function Chat() {
+  return (
+   <h1>Chat</h1>
+  );
+}
---
+++ src/views/SignIn.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+
+export default function SignIn() {
+  return (
+    <h1>Test</h1>
+  );
+}
