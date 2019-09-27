81d1afc54dac40128e280f29784ea28756989514
HenriqueFutema <minifut0407@gmail.com>
2019-09-27 20:57:26 -0300

    Reestruturação

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
-import SignIn from './views/SignIn'
 import Chat from './views/Chat'
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path='/' component={SignIn} exact/>
-        <Route path='/chat' component={Chat} exact/>
+        <Route path='/' component={Chat} exact/>
 
       </Switch>
     </BrowserRouter>
--- src/components/ChatComponent.js
+++ src/components/ChatComponent.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
 
-import './style2.css';
-
 import api from '../services/api'
 
 export default class ChatComponent extends Component {
